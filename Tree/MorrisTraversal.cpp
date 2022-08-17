8bc84edaa7aef444562c87251854c49f5984cda6
Jyotirmoy <subhodas45036@gmail.com>
2022-08-18 00:04:40 +0530

    Added Morris Traversal and Flatten Binary Tree to make a LL

---
+++ Tree/MorrisTraversal.cpp
@@ -0,0 +1,168 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node* insertBST(Node *root, int val)
+{
+    if(root == NULL)
+    {
+        return new Node(val);
+    }
+    if(val < root->data)
+    {
+        root->left = insertBST(root->left, val);
+    }
+    else if(val > root->data)
+    {
+        root->right = insertBST(root->right, val);
+    }
+    return root;
+}
+Node* searchBST(Node *root, int val)
+{
+    Node *temp = NULL;
+    if(root->data == val)
+    {
+        return root;
+    }
+    else if(root->data > val)
+    {
+        temp = searchBST(root->left, val);
+    }
+    else if(root->data < val)
+    {
+        temp = searchBST(root->right, val);
+    }
+    return temp;
+}
+Node* inorderSuccesorDel(Node *root)
+{
+    Node *current = root;
+    while(current && current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
+Node* inorderPredecessor(Node *root)
+{
+    Node *current = root;
+    while(current && current->right != NULL)
+    {
+        current = current->right;
+    }
+    return current;
+}
+Node* deleteBST(Node *root, int val)
+{
+    if(val < root->data)
+    {
+        root->left = deleteBST(root->left, val);
+    }
+    else if(val > root->data)
+    {
+        root->right = deleteBST(root->right, val);
+    }
+    else
+    {
+        if(root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        if(root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node *temp = inorderSuccesorDel(root->right);
+        root->data = temp->data;
+        free(temp);
+    }
+    return root;
+}
+void inorderMorrisTraversal(Node *root)
+{
+    Node *current = root;
+    while(current != NULL)
+    {
+        if(current->left == NULL)
+        {
+            cout << current->data << " ";
+            current = current->right;
+        }
+        else
+        {
+            Node *pred = current->left;
+            while(pred->right != NULL && pred->right != current)
+            {
+                pred = pred->right;
+            }
+            if(pred->right == NULL)
+            {
+                pred->right = current;
+                current = current->left;
+            }
+            else
+            {
+                pred->right = NULL;
+                cout << current->data << " ";
+                current = current->right;
+            }
+        }
+    }
+}
+void preorderMorrisTraversal(Node *root)
+{
+    Node *current = root;
+    while(current != NULL)
+    {
+        if(current->left == NULL)
+        {
+            cout << current->data << " ";
+            current = current->right;
+        }
+        else
+        {
+            Node *pred = current->left;
+            while(pred->right != NULL && pred->right != current)
+            {
+                pred = pred->right;
+            }
+            if(pred->right == current)
+            {
+                pred->right = NULL;
+                current = current->right;
+            }
+            else
+            {
+                cout << current->data << " ";
+                pred->right = current;
+                current = current->left;
+            }
+        }
+    }
+}
+int main()
+{
+    Node *root = NULL;
+    root = insertBST(root, 4);
+    root = insertBST(root, 2);
+    root = insertBST(root, 3);
+    root = insertBST(root, 6);
+    root = insertBST(root, 5);
+    root = insertBST(root, 7);
+    root = insertBST(root, 1);
+    inorderMorrisTraversal(root);
+}
