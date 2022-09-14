97580198a7e461695aec15da5fd5444b96ed8781
Jyotirmoy <subhodas45036@gmail.com>
2022-09-14 20:18:16 +0530

    Added Jump Game, Kth Largest Element, Kth Smallest Element, Rotate Array by One, Sliding Window Maximum

---
+++ Array/KthLargestElement.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void kLargestElement(int arr[], int k)
+{
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for(int i=0; i<k; i++)
+	{
+		pq.push(arr[i]);
+	}
+	for(int i=k; i<6; i++)
+	{
+		if(arr[i] > pq.top())
+		{
+			pq.pop();
+			pq.push(arr[i]);
+		}
+	}
+	cout << pq.top();
+}
+int main() {
+	int arr[] = {3, 1, 5, 12, 2, 11};
+	kLargestElement(arr, 3);
+	return 0;
+}
---
+++ Array/RotateArrayByOne.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void rotateArraybyOne(int arr[], int n)
+{
+	int temp = arr[n-1];
+    for(int i=n-1; i>=0; i--)
+    {   
+        arr[i] = arr[i-1];
+    }
+    arr[0] = temp;
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " "; 
+    }
+}
+int main() {
+	int arr[] = {9, 8, 7, 6, 4, 2, 1, 3};
+	rotateArraybyOne(arr, sizeof(arr)/sizeof(int));
+    
+	return 0;
+}
+
