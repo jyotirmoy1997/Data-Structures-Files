f37ca781d68f8c8fd72eeb2a9cd09e948c28ee34
jyotirmoy1997 <subhodas45036@gmail.com>
2023-10-20 14:27:03 +0530

    Added Assign Cookies, Fractional Knapsack

---
+++ Greedy Algorithms/AssignCookies.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findContentChildren(vector<int>& g, vector<int>& s){
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    int i = 0, j = 0, count = 0, n1 = g.size(), n2 = s.size();
+    while(i < n1 && j < n2){
+        if(g[i] <= s[j]){
+            i++;
+            j++;
+            count++;
+        }
+        else
+            j++;
+    }
+    return count;
+}
+int main(){
+    vector<int> g = {10, 9, 8, 7};
+    vector<int> s = {5, 6, 7, 8};
+
+    cout << findContentChildren(g, s);
+}
