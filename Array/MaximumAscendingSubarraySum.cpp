5696a6f09a623c11037a249191583761cbcb2d70
jyotirmoy1997 <subhodas45036@gmail.com>
2025-02-04 20:41:45 +0530

    Added Maximum Ascending Subarray Sum

---
+++ Array/MaximumAscendingSubarraySum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxAscendingSum(vector<int>& nums) {
+    int currentSum = nums[0], maxSum = currentSum;
+    
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[i - 1]) {
+            currentSum += nums[i];
+        } else {
+            currentSum = nums[i];
+        }
+        maxSum = max(maxSum, currentSum);
+    }
+    return maxSum;
+}
+int main(){
+    vector<int> nums = {10,20,30,5,10,50};
+    cout << maxAscendingSum(nums);
+}
